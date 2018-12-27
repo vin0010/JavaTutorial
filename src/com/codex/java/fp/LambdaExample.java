d21ccc6d90eb5a356284f9ad36ebbafa385c0770
Vinoth G <vin10catchme@gmail.com>
2018-12-27 18:28:03 +0530

    Added lambda function examples

--- src/com/codex/java/fp/LambdaExample.java
+++ src/com/codex/java/fp/LambdaExample.java
@@ -1,10 +1,21 @@
 package com.codex.java.fp;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.IntToDoubleFunction;
+
 public class LambdaExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		IntToDoubleFunction doubleFunction = time -> time;
+		System.out.println(doubleFunction.applyAsDouble(10));
+		Function<String , Integer> wordCount = s -> s.split(" ").length;
+		System.out.println(wordCount.apply("vinoth is a legend"));
+		BiFunction<String, Integer, Boolean> exceedsMaxLength = (s, maxLength) -> {
+			int actualLength = s.length();
+			return actualLength>maxLength;
+		};
+		System.out.println(exceedsMaxLength.apply("vinoth", 15));
 	}
 
 }
